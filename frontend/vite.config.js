7a9529ef714bdb6ac82207a07e0a01b346121daa
@muqarram.asad <mukarram.asad@hysabkytab.com>
2024-10-14 15:45:10 +0500

    add build command in vite config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,6 +12,8 @@ export default defineConfig({
         changeOrigin: true
       }
     }
-  }
-
+  },
+  build: {
+    outDir: 'dist',  // Ensure this matches the expected output directory
+  },
 })
