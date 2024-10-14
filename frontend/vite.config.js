86d120b15e2a35cb2364d84a38142b004a4c20fe
@muqarram.asad <mukarram.asad@hysabkytab.com>
2024-10-14 16:50:11 +0500

    quarter-final changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
   server: {
     port: 3000,
@@ -12,8 +15,6 @@ export default defineConfig({
         changeOrigin: true
       }
     }
-  },
-  build: {
-    outDir: 'dist',  // Ensure this matches the expected output directory
-  },
+  }
+ 
 })
